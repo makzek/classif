4286dcf23e4bb4067fdf79063de552eb940c35cb
Igor Ivanov <iivanov@sinergo.ru>
2017-08-07 13:12:31 +0700

    added REST example

---
+++ src/app/core/models/ViewModelResponse.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+import { iEnt } from './../services/pipRX.service';
+
+export class ViewModelResponse {
+    value: any;
+    formatters: any[];
+    contentTypes: any[];
+    declaredType: any;
+    statusCode: number;
+}
+
+export class DELIVERY_CL implements iEnt {
+	ISN_LCLASSIF: any;
+	CLASSIF_NAME: any;
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    template: '<iframe src="http://localhost/v175/main.aspx?login=tver&pass=tver">LOGIN</iframe>'
+})
+export class LoginComponent {
+
+}
