35652a77e9976522b5286903e033a34b49288448
Ekaterina <ekondratenko@sinrgo.ru>
2017-08-08 16:02:09 +0700

    add create node func, add show/hide deleted func, add change page in goNext(), add message at delte node func

--- src/app/selected-node/selected-node.component.ts
+++ src/app/selected-node/selected-node.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
 
 import { EosDictService } from '../services/eos-dict.service';
+import { EosMessageService } from '../services/eos-message.service';
+import { EosUserSettingsService } from '../services/eos-user-settings.service';
 import { EosDictionaryNode } from '../core/eos-dictionary-node';
 import { EosDictionary } from '../core/eos-dictionary';
 
@@ -13,14 +17,45 @@ export class SelectedNodeComponent {
 
     selectedNode: EosDictionaryNode;
     openedNode: EosDictionaryNode;
+    childrenListPerPage: EosDictionaryNode[];
 
     dictionary: EosDictionary;
 
     checkAll = false;
 
+    totalItems: number;
+    itemsPerPage: number = 10;
+
+    currentPage = 1;
+    pageAtList = 1;
+
     private _dictionaryId: string;
 
-    constructor(private _eosDictService: EosDictService, private router: Router) {
+    modalRef: BsModalRef;
+
+    newNode = new EosDictionaryNode({
+        id: null,
+        code: null,
+        title: null,
+        parentId: null,
+        parent: null,
+        children: [],
+        description: null,
+        isNode: null,
+        hasSubnodes: null,
+        isExpanded: null,
+        isDeleted: false,
+        selected: false,
+        data: null,
+    });
+
+    showDeleted: boolean;
+
+    constructor(private _eosDictService: EosDictService, 
+        private _eosMessageService: EosMessageService, 
+        private router: Router, 
+        private modalService: BsModalService,
+        private _eosUserSettingsService: EosUserSettingsService) {
         this._eosDictService.dictionary$.subscribe(
             (dictionary) => {
                 this.dictionary = dictionary;
@@ -39,6 +74,8 @@ export class SelectedNodeComponent {
                         this.checkAllItems(false);
                     }
                     this.openFullInfo(node.id);
+                    this.totalItems = node.children.length;
+                    this.childrenListPerPage = node.children.slice(0, this.itemsPerPage);
                 } else {
                     if (this.dictionary) {
                         this.selectedNode = this.dictionary.root;
@@ -56,6 +93,10 @@ export class SelectedNodeComponent {
             },
             (error) => alert(error)
         );
+
+        this._eosUserSettingsService.settings.subscribe((res) => {
+            this.showDeleted = res.find((s) => s.id === 'showDeleted').value;
+        }); 
     }
 
     openFullInfo(childId: string): void {
@@ -103,6 +144,7 @@ export class SelectedNodeComponent {
     }
 
     deleteSelectedItems(): void {
+        
         if (this.openedNode.selected && this.openedNode !== this.selectedNode) {
             this.openFullInfo(this.openedNode.parent.id);
         }
@@ -113,7 +155,7 @@ export class SelectedNodeComponent {
             } else {
                 this.selectNode('');
             }
-            return;
+           return;
         }
         const selectedNodes: string[] = [];
         this.selectedNode.children.forEach((child) => {
@@ -121,8 +163,28 @@ export class SelectedNodeComponent {
                 selectedNodes.push(child.id);
             }
         });
+        if (!selectedNodes.length ) {
+            this._eosMessageService.addNewMessage({
+                type: 'warning',
+                title: 'Ошибка удаления: ',
+                msg: 'не выбраны элементы для удаления',
+            });
+        }
         this._eosDictService.deleteSelectedNodes(this._dictionaryId, selectedNodes);
+        this.checkAllItems(false);
     }
+
+    createItem() {
+        this.modalRef.hide();
+        this._eosDictService.addChild(this.newNode);
+    }
+
+    openCreatingForm(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template);
+        this.newNode.parentId = this.selectedNode.id;
+        this.newNode.parent = this.selectedNode;
+    }
+
     nextItem(goBack: boolean): void {
         if (this.selectedNode.id !== this.openedNode.id) {
             let i = 0;
@@ -135,10 +197,34 @@ export class SelectedNodeComponent {
             if (goBack) {
                 this._eosDictService.openNode(this._dictionaryId, this.selectedNode.children[(i - 1 +
                     this.selectedNode.children.length) % this.selectedNode.children.length].id);
+                this.currentPage = Math.floor(((i - 1 + this.selectedNode.children.length) % this.selectedNode.children.length) / (this.itemsPerPage)) + 1;
             } else {
                 this._eosDictService.openNode(this._dictionaryId, this.selectedNode.children[(i + 1 +
                     this.selectedNode.children.length) % this.selectedNode.children.length].id);
+                this.currentPage = Math.floor(((i + 1 + this.selectedNode.children.length) % this.selectedNode.children.length) / (this.itemsPerPage)) + 1;
             }
         }
+    }
+
+    pageChanged(event: any): void {
+        this.pageAtList = 1;
+        this.childrenListPerPage = this.selectedNode.children.slice((event.page-1)*event.itemsPerPage, event.page*event.itemsPerPage);
+    }
+
+    setItemCount(value: string) {
+        this.itemsPerPage = +value;
+        this.childrenListPerPage = this.selectedNode.children.slice((this.currentPage-1)*+value, this.currentPage*+value);
+    }
+
+    showMore() {
+        this.pageAtList++;
+        this.childrenListPerPage = this.selectedNode.children.slice((this.currentPage-1)*this.itemsPerPage, this.currentPage*this.itemsPerPage*this.pageAtList);
+    }
+
+    switchShowDeleted(value: boolean) {
+        this._eosUserSettingsService.saveShowDeleted(value);
+    }
+
+    physicallyDelete() {
     }
 }
