4b8fac6e13d49c46aa957bf7eea29482027b83b7
Ekaterina <ekondratenko@sinrgo.ru>
2017-08-28 13:28:49 +0700

    move all color defenitions in theme.scss

--- src/app/services/eos-dict.service.ts
+++ src/app/services/eos-dict.service.ts
@@ -92,7 +92,7 @@ export class EosDictService {
                     this._api.getDictionaryMocked(dictionaryId)
                         .then((data: any) => {
                             _dictionary = new EosDictionary(new DictionaryDescriptor(BASIC_DICT), data);
-                            // _dictionary = new EosDictionary(new DictionaryDescriptor(DEPARTMENTS_DICT), data);
+                            //_dictionary = new EosDictionary(new DictionaryDescriptor(DEPARTMENTS_DICT), data);
                             console.log('_dictionary', _dictionary);
                             this._dictionary = _dictionary;
                             return this._api.getDictionaryNodesMocked(dictionaryId);
