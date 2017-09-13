62f15c8199a6064ad2e17c4d1a8b3328dc6f33fb
Igor Ivanov <iivanov@sinergo.ru>
2017-09-13 16:34:44 +0700

    re #EOS-188
    handled add API errors as auth errors
    implementing eos-dictionary module

--- src/eos-rest/index.ts
+++ src/eos-rest/index.ts
@@ -1,2 +1,5 @@
 export * from './interfaces/interfaces';
 export { PipRX } from './services/pipRX.service';
+export { RubricService } from './services/rubric.service';
+export { AuthService } from './services/auth.service';
+export { DepartmentService } from './services/department.service';
