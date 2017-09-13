6216117f7f05777328117c4a97b17c953dcfe91a
Ekaterina <ekondratenko@sinrgo.ru>
2017-09-13 10:16:53 +0700

    fix rooms-card-edit and department-card-edit with new action service

--- src/app/edit-card/action.service.ts
+++ src/app/edit-card/action.service.ts
@@ -2,28 +2,42 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Observable } from 'rxjs/Observable';
 
+
+export enum EDIT_CARD_ACTIONS {
+    create,
+    save,
+    cancel,
+    makeEmptyObject
+};
+
+export enum EDIT_CARD_MODES {
+    edit,
+    view,
+    unsavedChanges
+};
+
 @Injectable() export class EditCardActionService {
-    private _action$: BehaviorSubject<string>;
-    private _mode$: BehaviorSubject<string>;
+    private _action$: BehaviorSubject<EDIT_CARD_ACTIONS>;
+    private _mode$: BehaviorSubject<EDIT_CARD_MODES>;
 
     constructor() {
-        this._action$ = new BehaviorSubject<string>(null);
-        this._mode$ = new BehaviorSubject<string>(null);
+        this._action$ = new BehaviorSubject<EDIT_CARD_ACTIONS>(null);
+        this._mode$ = new BehaviorSubject<EDIT_CARD_MODES>(null);
     }
 
-    get action$(): Observable<string> {
+    get action$(): Observable<EDIT_CARD_ACTIONS> {
         return this._action$.asObservable();
     }
 
-    get mode$(): Observable<string> {
+    get mode$(): Observable<EDIT_CARD_MODES> {
         return this._mode$.asObservable();
     }
 
-    emitAction(value: string) {
+    emitAction(value: EDIT_CARD_ACTIONS) {
         this._action$.next(value);
     }
 
-    emitMode(value: string) {
+    emitMode(value: EDIT_CARD_MODES) {
         this._mode$.next(value);
     }
 }
