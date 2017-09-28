99f004b51d4d639bcad867eeea5cf58a1a1bd907
Ekaterina <ekondratenko@sinrgo.ru>
2017-09-28 18:19:19 +0700

    redesign departmants card partly and add validation in it

--- src/eos-dictionaries/card-views/rubricator-card-edit.component.ts
+++ src/eos-dictionaries/card-views/rubricator-card-edit.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 
 import { CardEditComponent } from './card-edit.component';
-import { TITLE_LENGTH, DESCRIPTION_LENGTH } from '../consts/input-validation';
 
 @Component({
     selector: 'eos-rubricator-card-edit',
     templateUrl: 'rubricator-card-edit.component.html',
 })
 export class RubricatorCardEditComponent extends CardEditComponent {
-
-    readonly titleLenth = TITLE_LENGTH;
-    readonly descriptionLength = DESCRIPTION_LENGTH;
 
      constructor() {
         super();
