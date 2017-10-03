bcf2c2f824074ba675e5c17b417bc6c180961610
Люкевич Василий Николаевич <lima@eos.loc>
2017-10-03 16:30:45 +0300

    загрузка изобращение в БД

--- src/eos-common/photo-uploader/photo-uploader.component.ts
+++ src/eos-common/photo-uploader/photo-uploader.component.ts
@@ -2,6 +2,9 @@ import { Component, EventEmitter, Output, ViewChild, ElementRef, Input, OnInit }
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { ModalDirective } from 'ngx-bootstrap/modal';
+import { PipRX} from '../../eos-rest/services/pipRX.service';
+import { Utils } from '../../eos-rest/core/utils';
+import {DELO_BLOB} from '../../eos-rest/interfaces/structures';
 
 @Component({
     selector: 'eos-photo-uploader',
@@ -16,7 +19,7 @@ export class PhotoUploaderComponent implements OnInit {
     @ViewChild('fileInput') inputEl: ElementRef;
     @ViewChild('confirmModal') private confirmModalRef: ModalDirective;
 
-    contactUrl = 'http://lalala';
+    contactUrl = 'http://localhost/Eos.Delo.OData/Services/DELO_BLOB.asmx/Upload';
     uploading = false;
 
     imageSrc = '';
@@ -26,7 +29,7 @@ export class PhotoUploaderComponent implements OnInit {
     // fileCount: number;
     file: File;
 
-    constructor(private _http: Http) {
+    constructor(private _http: Http, private pip: PipRX) {
     }
 
     ngOnInit() {
@@ -55,10 +58,34 @@ export class PhotoUploaderComponent implements OnInit {
             for (let i = 0; i < this.fileCount; i++) {
                 formData.append('file[]', this.nativeInputEl.files.item(i));
             }*/
-            formData.append('file[]', this.file);
+            let s = this.imageSrc;
+            const pos = s.indexOf(',') + 1;
+            // убрать последнюю скобку и преамбулу
+            s = s.substr(pos, s.length - pos - 1 );
+            s = s.replace(/\s/g, '+');
+            // TODO: из преамбулы получить правильное расширение файла
+
+            const delo_blob = this.pip.prepareAdded<DELO_BLOB>({
+                ISN_BLOB: this.pip.sequenceMap.GetTempISN(),
+                EXTENSION: 'PNG' // TODO: правиольное расширение файла указать сюда
+            }, 'DELO_BLOB');
+
+            const chl = Utils.changeList([delo_blob]);
+
+            const content = { isn_target_blob: delo_blob.ISN_BLOB, data: s};
+            Utils.invokeSop(chl, 'DELO_BLOB_SetDataContent', content);
+
+
+            this.pip.batch(chl, '').subscribe(data => {
+                alert(this.pip.sequenceMap.GetFixed(delo_blob.ISN_BLOB));
+            })
+
+            /*formData.append('file', this.file);
             this._http
                 .post(this.contactUrl, formData).subscribe(
                 data => {
+                    // tslint:disable-next-line:no-debugger
+                    debugger;
                     console.log('success');
                     this.uploading = false;
                     this.endUploading.emit(data);
@@ -67,7 +94,7 @@ export class PhotoUploaderComponent implements OnInit {
                     console.log(error);
                     this.uploading = false;
                     this.endUploading.emit(null);
-                })
+                })*/
         // }
         this.nativeInputEl.value = null;
     }
@@ -80,6 +107,6 @@ export class PhotoUploaderComponent implements OnInit {
     private _handleReaderLoaded(e) {
         const reader = e.target;
         this.file = reader.result;
-        this.imageSrc = 'url(' + reader.result + ')'
+        this.imageSrc = 'url(' + reader.result + ')';
     }
 }
