9e545583b7db0731739b8986f1b7a1fe4089f9ce
Andrey Kernichniy <akernichniy@sinergo.ru>
2017-10-23 20:18:31 +0700

    eos-251 fix button position iwth modal windows

--- src/eos-dictionaries/consts/confirm.consts.ts
+++ src/eos-dictionaries/consts/confirm.consts.ts
@@ -1,7 +1,7 @@
 import { IConfirmWindow } from '../../eos-common/core/confirm-window.interface';
 
 export const CONFIRM_SAVE_ON_LEAVE: IConfirmWindow = {
-    title: 'Имеются не сохранённые изменения!',
+    title: 'Имеются не сохранённые изменения',
     body: 'Внесенные изменения могут быть потеряны.',
     okTitle: 'Сохранить и закрыть',
     cancelTitle: 'Закрыть без сохранения'
