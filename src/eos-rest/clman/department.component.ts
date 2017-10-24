da7feb51063e212efbb63bd03d57365b803127a7
Igor Ivanov <iivanov@sinergo.ru>
2017-10-24 16:36:05 +0700

    build fixes

--- src/eos-rest/clman/department.component.ts
+++ src/eos-rest/clman/department.component.ts
@@ -25,56 +25,61 @@ class vmDEPARTMENT {
 
         // загружаем от корня дерева до запрашиваемой вершины
         // поскольку органицация, картотека и чтото еще могут быть определены выше по дереву
-        const rDeps = Observable.fromPromise(pip.read<DEPARTMENT>({ DEPARTMENT: treeDues(due), orderby: 'DUE' }).toPromise()) ;
+        const rDeps = Observable.fromPromise(pip.read<DEPARTMENT>({ DEPARTMENT: treeDues(due), orderby: 'DUE' }).toPromise());
         // TODO: разобраться почему без прохода через промис запрос выполняется дважды
         // или Pipe для крепкости переписать на промисы
 
         // ищем пользователя ассоцированного с показываемым дл
-        const rUser = pip.read<USER_CL>({USER_CL : PipRX.criteries({DUE_DEP: due})});
-        const rDopInfo = rDeps.flatMap( d => {
+        const rUser = pip.read<USER_CL>({ USER_CL: PipRX.criteries({ DUE_DEP: due }) });
+        const rDopInfo = rDeps.flatMap(d => {
             const l = d.length - 1;
             // загружаем доп. инфо о ДЛ и подразделении
-            const rPrintInfo = pip.read<CB_PRINT_INFO>({ CB_PRINT_INFO: PipRX.criteries({
-                ISN_OWNER: d[l].ISN_NODE.toString() + '|' + d[l].ISN_HIGH_NODE.toString(),
-                OWNER_KIND: '104' })});
+            const rPrintInfo = pip.read<CB_PRINT_INFO>({
+                CB_PRINT_INFO: PipRX.criteries({
+                    ISN_OWNER: d[l].ISN_NODE.toString() + '|' + d[l].ISN_HIGH_NODE.toString(),
+                    OWNER_KIND: '104'
+                })
+            });
             let rOrg = Observable.of([]);
             for (let i = l; i !== -1; i--) {
-                if ( d[i].DUE_LINK_ORGANIZ !== null) {
-                    rOrg = pip.read<ORGANIZ_CL>({ORGANIZ_CL: d[i].DUE_LINK_ORGANIZ});
+                if (d[i].DUE_LINK_ORGANIZ !== null) {
+                    rOrg = pip.read<ORGANIZ_CL>({ ORGANIZ_CL: d[i].DUE_LINK_ORGANIZ });
                 }
             }
-            const rCab = d[l].ISN_CABINET === null ? Observable.of([]) : pip.read<CABINET>({CABINET: d[l].ISN_CABINET});
-            return Observable.combineLatest(rPrintInfo, rOrg, rCab) ;
+            const rCab = d[l].ISN_CABINET === null ? Observable.of([]) : pip.read<CABINET>({ CABINET: d[l].ISN_CABINET });
+            return Observable.combineLatest(rPrintInfo, rOrg, rCab);
         });
         // загружаем индекс СЭВ
-        const rSevIndex = pip.read<SEV_ASSOCIATION>({SEV_ASSOCIATION: PipRX.criteries({
-            OBJECT_NAME: 'DEPARTMENT', OBJECT_ID : due
-        })})
+        const rSevIndex = pip.read<SEV_ASSOCIATION>({
+            SEV_ASSOCIATION: PipRX.criteries({
+                OBJECT_NAME: 'DEPARTMENT', OBJECT_ID: due
+            })
+        })
         return Observable.combineLatest(rDeps, rUser, rDopInfo, rSevIndex)
-        .map(([a, b, dop, d]) => {
-            const result = new vmDEPARTMENT();
-            result.row = a[a.length - 1];
-            // tslint:disable-next-line:no-debugger
-            // debugger;
+            .map(([a, b, dop, d]) => {
+                const result = new vmDEPARTMENT();
+                result.row = a[a.length - 1];
+                // tslint:disable-next-line:no-debugger
+                // debugger;
 
-            // TODO: решить нужно ли делать стабы, пока сделал
-            // решил делать стабы(запись со всеми нужными properties), тогда биндинг проще
-            // и можно сразу разобраться, что делать
-            // ничего - если не заполнили ценных полей или ничего не изменили
-            // INSERT, если записи нет, но чтото заполнили
-            // UPDATE, если чтото изменили
-            result.CB_PRINT_INFO = pip.entityHelper.prepareForEdit<CB_PRINT_INFO>( dop[0][0], 'CB_PRINT_INFO' );
-            result.ORGANIZ = pip.entityHelper.prepareForEdit<ORGANIZ_CL>(dop[1][0], 'ORGANIZ_CL' );
-            // Здесь врапить не надо - запись кабинете мы редатировать не должны
-            // а ссылку на него можно и по другому поставить.
-            result.cabinet = <CABINET>dop[2][0];
+                // TODO: решить нужно ли делать стабы, пока сделал
+                // решил делать стабы(запись со всеми нужными properties), тогда биндинг проще
+                // и можно сразу разобраться, что делать
+                // ничего - если не заполнили ценных полей или ничего не изменили
+                // INSERT, если записи нет, но чтото заполнили
+                // UPDATE, если чтото изменили
+                result.CB_PRINT_INFO = pip.entityHelper.prepareForEdit<CB_PRINT_INFO>(dop[0][0], 'CB_PRINT_INFO');
+                result.ORGANIZ = pip.entityHelper.prepareForEdit<ORGANIZ_CL>(dop[1][0], 'ORGANIZ_CL');
+                // Здесь врапить не надо - запись кабинете мы редатировать не должны
+                // а ссылку на него можно и по другому поставить.
+                result.cabinet = <CABINET>dop[2][0];
 
-            result.SEV_ASSOCIATION = pip.entityHelper.prepareForEdit<SEV_ASSOCIATION>(d[0], 'SEV_ASSOCIATION');
+                result.SEV_ASSOCIATION = pip.entityHelper.prepareForEdit<SEV_ASSOCIATION>(d[0], 'SEV_ASSOCIATION');
 
-            console.log(JSON.stringify(result));
+                console.log(JSON.stringify(result));
 
-            return result;
-        }).toPromise();
+                return result;
+            }).toPromise();
     }
 }
 
@@ -83,7 +88,7 @@ class vmDEPARTMENT {
     selector: 'eos-rest-department',
     templateUrl: './department.component.html'
 })
-export class DepartmentComponent  implements OnInit {
+export class DepartmentComponent implements OnInit {
     treeItems: DEPARTMENT[] = [];
     listItems: DEPARTMENT[] = [];
     currentListItem: DEPARTMENT;
@@ -118,10 +123,11 @@ export class DepartmentComponent  implements OnInit {
     onSelectListItem(cur: DEPARTMENT): void {
         this.currentListItem = cur;
         vmDEPARTMENT.Load(this.pip, cur.DUE, false)
-            .then( vm => {this.detailedItem = vm});
+            .then(vm => { this.detailedItem = vm });
     }
 
-/*    onAdd() {
+    onAdd() {
+        /*
         const tisn = this.pip.sequenceMap.GetTempISN();
         const tmp = this.pip.prepareAdded<DEPARTMENT>( {
             DUE: this.currentItem.DUE + tisn + '.',
@@ -129,16 +135,17 @@ export class DepartmentComponent  implements OnInit {
             CLASSIF_NAME: 'Добавляем?'
         }, 'DEPARTMENT');
         this.currentItem = tmp;
+        */
     }
 
     onSave() {
+        /*
         const chl = Utils.changeList([this.currentItem]);
         this.pip.batch(chl, '').subscribe((r) => {
             alert(this.pip.sequenceMap.GetFixed(this.currentItem.DUE) + ' ' + this.pip.sequenceMap.GetFixed(this.currentItem.ISN_NODE));
         });
-
+        */
     }
-*/
 }
 
 
