024cb965243e0ab62c8a74a5dd0560c4682591b1
Igor Ivanov <iivanov@sinergo.ru>
2017-10-26 19:21:01 +0700

    re #EOS-306
    auth changes
    added IDesk interface

--- src/eos-rest/services/appContext.service.ts
+++ src/eos-rest/services/appContext.service.ts
@@ -22,7 +22,10 @@ export class AppContext {
     init(): Promise<any> {
         const p = this.pip;
         // раз присоеденились сбрасываем подавление ругательства о потере соединения
+        // @igiware: потенциальная ошибка, тк PipeRX - singleton, параллельный запрос данных пропустит ошибку,
+        //           как и последующие дальнейшие запросы
         p.errorService.LostConnectionAlerted = false;
+
         const oSysParams = p.read<SYS_PARMS>({
             SysParms: ALL_ROWS,
             _moreJSON: {
@@ -31,23 +34,25 @@ export class AppContext {
                 ParamsDic: '-99'
             }
         });
+
         const oCurrentUser = p.read<USER_CL>({
             CurrentUser: ALL_ROWS,
             expand: 'USERDEP_List,USERSECUR_List',
             _moreJSON: { ParamsDic: null }
         });
 
-        const result = Promise.all([oSysParams, oCurrentUser])
-        .then(([sysParms, curentUser]) => {
-            this.SysParms = sysParms[0];
-            this.CurrentUser = curentUser[0];
-            return 'all readed';
-        });
-        result.then(d => {
-            // tslint:disable-next-line:no-debugger
-            debugger;
-        });
-        return result;
+        return Promise.all([oSysParams, oCurrentUser])
+            .then(([sysParms, curentUser]) => {
+                this.SysParms = sysParms[0];
+                this.CurrentUser = curentUser[0];
+                return [this.CurrentUser, this.SysParms];
+            })
+            /*
+            .then(d => {
+                // tslint:disable-next-line:no-debugger
+                debugger;
+            })*/
+            ;
     }
 
     reInit() {
--- src/eos-rest/services/auth.service.ts
+++ src/eos-rest/services/auth.service.ts
@@ -4,12 +4,11 @@ import { Http } from '@angular/http';
 import { PipRX } from './pipRX.service';
 import { ApiCfg } from '../core/api-cfg';
 import { HTTP_OPTIONS } from '../core/consts';
-import { AppContext} from './appContext.service';
+import { AppContext } from './appContext.service';
 
 
 @Injectable()
 export class AuthService {
-
     private _cfg: ApiCfg;
 
     constructor(private _http: Http, private _pipe: PipRX, private appCtx: AppContext) {
@@ -18,14 +17,8 @@ export class AuthService {
 
     public login(user: string, passwd: string): Promise<any> {
         const _url = this._cfg.authSrv + 'Login?app=api&' + 'username=' + user + '&pass=' + passwd;
-        const r = this._http.get(_url, HTTP_OPTIONS).toPromise<any>()
-            .then( d => {
-            // tslint:disable-next-line:no-debugger
-            /* debugger; */
-           return this.appCtx.init();
-        });
-
-        return r;
+        return this._http.get(_url, HTTP_OPTIONS).toPromise<any>()
+            .then((d) => this.appCtx.init());
     }
 
     public logout() {
