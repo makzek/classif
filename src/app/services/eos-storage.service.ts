75eddd4e27bf7bbdcf1b4bcd6eb97cae05263258
Andrey Kernichniy <akernichniy@sinergo.ru>
2017-12-04 15:13:59 +0700

    EOS-177 cancel bad change

--- src/app/services/eos-storage.service.ts
+++ src/app/services/eos-storage.service.ts
@@ -38,7 +38,7 @@ export class EosStorageService {
      * @param key key for data
      */
     public getItem(key: string): any {
-        if (key && key !== '__storage') {
+        if (key && key !== '__storage' && key !== 'userOrder') {
             return this._data[key];
         }
     }
