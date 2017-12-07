e4f6b05cd26f4530a3bfa8ff19c6bc5892842863
Ekaterina <ekondratenko@sinrgo.ru>
2017-12-07 18:09:38 +0700

    fix fullTitles and title for create

--- src/eos-dictionaries/card/card.component.ts
+++ src/eos-dictionaries/card/card.component.ts
@@ -389,10 +389,11 @@ export class CardComponent implements CanDeactivateGuard, OnInit, OnDestroy {
             .then((resp: EosDictionaryNode) => {
                 this._msgSrv.addNewMessage(SUCCESS_SAVE);
                 const fullTitle = this._fullTitle(resp);
+                console.log('fullTitle', fullTitle);
                 this._deskSrv.addRecentItem({
                     url: this._router.url,
                     title: resp.data.rec.CLASSIF_NAME,
-                    fullTitle: fullTitle + ' - Редактирование'
+                    fullTitle: fullTitle
                 });
                 this._clearEditingCardLink();
                 return resp;
@@ -402,12 +403,13 @@ export class CardComponent implements CanDeactivateGuard, OnInit, OnDestroy {
 
     private _fullTitle(node: EosDictionaryNode) {
         let parent = node.parent;
-        let arr = [node.data.CLASSIF_NAME];
+        let arr = [node.data.rec.CLASSIF_NAME];
         while (parent.parent) {
-            arr.push(parent.data.CLASSIF_NAME);
+            arr.push(parent.data.rec.CLASSIF_NAME);
             parent = parent.parent;
         }
-        arr.push(parent.data.RUBRIC_CODE);
+        arr.push(parent.data.rec.RUBRIC_CODE);
+        arr.push('Справочники');
         arr = arr.reverse();
         const fullTItle = arr.join('/');
         return fullTItle;
