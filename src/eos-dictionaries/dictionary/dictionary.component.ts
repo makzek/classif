217d47c35734eef4c2dba9bd2c4c4e180e0294ed
Ekaterina <ekondratenko@sinrgo.ru>
2017-12-12 17:42:37 +0700

    remove fon service, make fake padding with span with dinamic width, fix styles to this case

--- src/eos-dictionaries/dictionary/dictionary.component.ts
+++ src/eos-dictionaries/dictionary/dictionary.component.ts
@@ -20,7 +20,6 @@ import { EosDictionaryNode } from '../core/eos-dictionary-node';
 import { EosMessageService } from '../../eos-common/services/eos-message.service';
 import { EosStorageService } from '../../app/services/eos-storage.service';
 import { EosSandwichService } from '../services/eos-sandwich.service';
-import { EosActiveTreeNodeService } from '../tree/active-node-fon.service';
 
 import { E_FIELD_SET, IFieldView } from '../core/dictionary.interfaces';
 import { INodeListParams } from '../core/node-list.interfaces';
@@ -119,19 +118,7 @@ export class DictionaryComponent implements OnDestroy, DoCheck, AfterViewInit {
         private _deskSrv: EosDeskService,
         private _confirmSrv: ConfirmWindowService,
         private _sandwichSrv: EosSandwichService,
-        private _actTreeNodeSrv: EosActiveTreeNodeService
     ) {
-        _actTreeNodeSrv.fon$.takeUntil(this.ngUnsubscribe)
-            .subscribe(fonConf => {
-                this.fonConf.width = fonConf.width + 'px';
-                this.fonConf.height = fonConf.height + 'px';
-                if (this.treeEl) {
-                    this.fonConf.top = fonConf.top + this.treeEl.nativeElement.scrollTop + 'px';
-                } else {
-                    this.fonConf.top = fonConf.top + 'px';
-                }
-            })
-
         _route.params.subscribe((params) => {
             if (params) {
                 this.dictionaryId = params.dictionaryId;
