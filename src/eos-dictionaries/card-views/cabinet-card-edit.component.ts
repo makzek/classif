2a1c3bd94f0d534c72aebc2be1824d78f8a571b3
Ekaterina <ekondratenko@sinrgo.ru>
2017-12-25 11:09:50 +0700

    change styles for edit card for make it suitable for cabinet card, add cabinet in dictionaries const, style first block and block divider in cabinet card

--- src/eos-dictionaries/card-views/cabinet-card-edit.component.ts
+++ src/eos-dictionaries/card-views/cabinet-card-edit.component.ts
@@ -3,10 +3,10 @@ import { Component, Injector } from '@angular/core';
 import { BaseCardEditComponent } from './base-card-edit.component';
 
 @Component({
-    selector: 'eos-rooms-card-edit',
-    templateUrl: 'rooms-card-edit.component.html',
+    selector: 'eos-cabinet-card-edit',
+    templateUrl: 'cabinet-card-edit.component.html',
 })
-export class RoomsCardEditComponent extends BaseCardEditComponent {
+export class CabinetCardEditComponent extends BaseCardEditComponent {
     showOwners = true;
     showUsers = true;
     showAccess = true;
