23eec079db99b8b7f2eef710d365b417a9315836
Igor Ivanov <iivanov@sinergo.ru>
2018-01-10 16:24:55 +0700

    re #EOS-525
    blocked cassade API call on logic deleting operations

--- src/eos-dictionaries/core/abstract-dictionary-descriptor.ts
+++ src/eos-dictionaries/core/abstract-dictionary-descriptor.ts
@@ -1,4 +1,4 @@
-import { E_DICT_TYPE, IDictionaryDescriptor, E_FIELD_SET, IRecordOperationResult  } from 'eos-dictionaries/interfaces';
+import { E_DICT_TYPE, IDictionaryDescriptor, E_FIELD_SET, IRecordOperationResult } from 'eos-dictionaries/interfaces';
 import { RecordDescriptor } from 'eos-dictionaries/core/record-descriptor';
 
 import { commonMergeMeta } from 'eos-rest/common/initMetaData';
@@ -161,7 +161,7 @@ export abstract class AbstractDictionaryDescriptor {
     markDeleted(records: any[], deletedState = 1, cascade = false): Promise<any[]> {
         records.forEach((record) => record.DELETED = deletedState);
         const changes = this.apiSrv.changeList(records);
-        if (cascade) {
+        if (1 !== 1 && cascade) { // blocked while cascade operations disabled
             PipRX.invokeSop(changes, 'ClassifCascade_TRule', { DELETED: 1 });
         }
         // console.log('markDeleted ', changes);
@@ -200,8 +200,8 @@ export abstract class AbstractDictionaryDescriptor {
                 }
             }
         });
-        console.log('originalData', originalData);
-        console.log('changeData', changeData);
+        // console.log('originalData', originalData);
+        // console.log('changeData', changeData);
         return this.apiSrv.batch(this.apiSrv.changeList(changeData), '');
         // return Promise.all(_res); // this._postChanges(originalData.rec, updates.rec);
     }
