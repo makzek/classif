9f97e9ed7744c558d0942cb1ef635d664b17bc8b
Andrey Kernichniy <akernichniy@sinergo.ru>
2018-01-10 17:36:09 +0700

    EOS -485 Path fixed

--- src/eos-dictionaries/node-info/node-info.component.ts
+++ src/eos-dictionaries/node-info/node-info.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { EosDictService } from '../services/eos-dict.service';
-import { IDictionaryViewParameters } from '../core/eos-dictionary.interfaces';
+import { IDictionaryViewParameters } from '../interfaces/eos-dictionary.interfaces';
 
 
 import { BaseNodeInfoComponent } from './base-node-info';
