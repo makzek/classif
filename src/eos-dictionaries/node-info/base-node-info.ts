801b92f22fe7b8cb2091978e533325db8d847bf0
Igor Ivanov <iivanov@sinergo.ru>
2018-01-22 19:21:00 +0700

    removed unused imports, constructor & poroperties

--- src/eos-dictionaries/node-info/base-node-info.ts
+++ src/eos-dictionaries/node-info/base-node-info.ts
@@ -1,10 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-import { EosDictService } from '../services/eos-dict.service';
 import { IFieldView, E_FIELD_TYPE, E_RECORD_ACTIONS } from 'eos-dictionaries/interfaces';
 import { RECORD_ACTIONS_EDIT, RECORD_ACTIONS_NAVIGATION_UP, RECORD_ACTIONS_NAVIGATION_DOWN } from '../consts/record-actions.consts';
-import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/takeUntil';
 
 export class BaseNodeInfoComponent {
     @Input() fieldsDescriptionShort: any;
@@ -13,15 +9,10 @@ export class BaseNodeInfoComponent {
     @Input() nodeDataFull: any;
     @Output() action: EventEmitter<E_RECORD_ACTIONS> = new EventEmitter<E_RECORD_ACTIONS>();
 
-    private ngUnsubscribe: Subject<any> = new Subject();
-
-    updating;
     actionEdit = RECORD_ACTIONS_EDIT;
     actionNavigationUp = RECORD_ACTIONS_NAVIGATION_UP;
     actionNavigationDown = RECORD_ACTIONS_NAVIGATION_DOWN;
     fieldTypes = E_FIELD_TYPE;
-
-    constructor() { }
 
     actionHandler(type: E_RECORD_ACTIONS) {
         this.action.emit(type);
