fed6db9320a99d52930a0fdcfcc92ef1c0ae3855
Igor Ivanov <iivanov@sinergo.ru>
2018-05-15 15:40:35 +0700

    re #EOS-810 added fullname into cabinet search

--- src/eos-dictionaries/consts/dictionaries/cabinet.consts.ts
+++ src/eos-dictionaries/consts/dictionaries/cabinet.consts.ts
@@ -63,7 +63,7 @@ export const CABINET_DICT: IDictionaryDescriptor = Object.assign({}, LINEAR_TEMP
     treeFields: ['CABINET_NAME'],
     listFields: ['CABINET_NAME', 'DEPARTMENT_NAME'],
     searchFields: ['CABINET_NAME'],
-    fullSearchFields: ['CABINET_NAME'],
+    fullSearchFields: ['CABINET_NAME', 'FULLNAME'],
     allVisibleFields: ['FULLNAME'],
     shortQuickViewFields: ['CABINET_NAME', 'FULLNAME'],
     quickViewFields: ['ISN_CABINET', 'CABINET_NAME', 'DEPARTMENT_NAME', 'department', 'owners', 'users'],
